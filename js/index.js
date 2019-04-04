199bb9c1fb587c81838b8e12573170202aca818c
rippera <mikheil.mezurnishvili10b@geolab.edu.ge>
2019-04-04 13:25:26 +0400

    no message

--- js/index.js
+++ js/index.js
@@ -7,10 +7,37 @@ $(document).ready(function(){
     });
 });
 
-window.addEventListener('load',setTimeout);
+document.addEventListener('load',setTimeout);
 setTimeout(socIconAnimation,2000);
 function socIconAnimation() {
     let socialWrapper = document.getElementById('social__wrapper');
     socialWrapper.style.left = '-5px';
     socialWrapper.classList.add('socAnimation');
 };
+
+function headerAnimations() {
+  let header = document.querySelector('.header__fixed');
+  let topPos = window.pageYOffset;
+  if (topPos > 100) {
+    header.style.background = 'rgba(0,0,0,0.9)';
+    header.style.padding    = '15px 0';
+  }else{
+    header.style.background = 'transparent';
+    header.style.padding    = '30px 0';
+  }
+}
+
+function goTop() {
+  let goTop = document.getElementById('gotop');
+  let topPos = window.pageYOffset;
+  if (topPos > 250){
+    goTop.style.opacity = '1';
+  }else{
+    goTop.style.opacity = '0';
+  }
+}
+window.addEventListener('scroll',function(){
+  headerAnimations();
+  goTop();
+});
+
